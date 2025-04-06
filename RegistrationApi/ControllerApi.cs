ff181f37df9d28c9a936cfde3fec19705d9c6bff
trokhin87 <trokhin87@gmail.com>
2025-04-06 15:38:49 +0300

    добавил

--- RegistrationApi/ControllerApi.cs
+++ RegistrationApi/ControllerApi.cs
@@ -9,7 +9,7 @@ using Swashbuckle.AspNetCore.Filters;
 namespace RegistrationApi;
 
 [ApiController]
-[Route("api")]
+[Route("api/Rgister")]
 public class ControllerApi : ControllerBase
 {
     private readonly IRegistrService _registrService;
